b50636910af7880219727b370b8f4446809f0b43
harinathreddy529 <harikotloni2001@gmail.com>
2023-12-14 11:19:38 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -2,14 +2,22 @@ from django.db import models
 
 # Create your models here.
 class topic(models.Model):
-    Topic_name=models.CharField(max_length=100,primary_key=True)
+    topic_name=models.CharField(max_length=100,primary_key=True)
+
+    def __str__(self):
+        return self.topic_name
 
 class webpage(models.Model):
     Topic_name=models.ForeignKey(topic,on_delete=models.CASCADE)
     name=models.CharField(max_length=100)
     url=models.URLField()
 
+    def __str__(self):
+        return self.name
+
 class accessrecord(models.Model):
     name=models.ForeignKey(webpage,on_delete=models.CASCADE)
     date=models.DateField()
     author=models.CharField(max_length=100)
+    def __str__(self):
+        return self.author
--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from app.models import *
 # Create your views here.
+def display_topics(request):
+    QLTO=topic.objects.all()
+    d={'topics':QLTO}
+    return render(request,'display_topics.html',d)
